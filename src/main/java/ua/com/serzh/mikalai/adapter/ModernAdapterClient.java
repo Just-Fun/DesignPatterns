cb64e063aa744f45930d2b9537bd152a04302ad5
Just-Fun <Just-Fun@users.noreply.github.com>
2016-09-13 16:09:08 +0300

    add Mikalai's patterns

--- src/main/java/ua/com/serzh/mikalai/adapter/ModernAdapterClient.java
+++ src/main/java/ua/com/serzh/mikalai/adapter/ModernAdapterClient.java
@@ -16,7 +16,6 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
-
 /**
  * @author Alimenkou Mikalai
  * @version 1.0
