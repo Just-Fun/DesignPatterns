10873da64847c479af0d0012592e453dff250354
Just-Fun <Just-Fun@users.noreply.github.com>
2016-09-29 16:33:17 +0300

    clean

--- src/main/java/ua/com/serzh/Temp.java
+++ src/main/java/ua/com/serzh/Temp.java
@@ -6,13 +6,5 @@ package ua.com.serzh;
 public class Temp {
     public static void main(String[] args) {
 
-        String s1 = "a";
-        String s2 = "b";
-
-        System.out.println(s1.hashCode());
-        System.out.println(s2.hashCode());
-
-        System.out.println(s1.charAt(0));
-        System.out.println(s2.charAt(0));
     }
 }
--- src/main/java/ua/com/serzh/Temp2.java
+++
@@ -1,9 +0,0 @@
-package ua.com.serzh;
-
-/**
- * Created by Serzh on 9/26/16.
- */
-public class Temp2 {
-    public static void main(String[] args) {
-    }
-}
