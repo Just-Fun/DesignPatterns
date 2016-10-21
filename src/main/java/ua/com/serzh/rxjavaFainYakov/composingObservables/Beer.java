f90ad5d3e38de9d100015aed92e44c21fddc36ae
Just-Fun <Just-Fun@users.noreply.github.com>
2016-10-21 12:06:29 +0300

    add Observer Fain

---
+++ src/main/java/ua/com/serzh/rxjavaFainYakov/composingObservables/Beer.java
@@ -0,0 +1,8 @@
+package ua.com.serzh.rxjavaFainYakov.composingObservables;
+
+public class Beer extends Drink {
+
+    Beer(String name, String country,float price){
+        super(name, country, price);
+    }
+}
---
+++ src/main/java/ua/com/serzh/rxjavaFainYakov/composingObservables/SoftDrink.java
@@ -0,0 +1,8 @@
+package ua.com.serzh.rxjavaFainYakov.composingObservables;
+
+public class SoftDrink extends Drink{
+
+    SoftDrink(String name, String country,float price){
+        super(name, country, price);
+    }
+}
