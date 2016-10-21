ea798cb06a1d43180134d55b1737a0dd2e2189b6
Just-Fun <Just-Fun@users.noreply.github.com>
2016-10-21 11:58:33 +0300

    add Observer

---
+++ src/main/java/ua/com/serzh/observer/sample/hard/Resume.java
@@ -0,0 +1,13 @@
+package ua.com.serzh.observer.sample.hard;
+
+import java.util.Set;
+
+public interface Resume {
+	
+	// в резюме есть контактные данные кандидата
+	Candidate getCandidate();
+	
+	// и набор технологий, которыми владеет кандидат
+	Set<String> getTechnologies();
+	
+}
